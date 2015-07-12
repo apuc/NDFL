2229fdf3191cae0219520fdfcd5a9ee5634be947
Kirill Bouko <apuc06@mail.ru>
2015-07-12 10:27:58 +0400

    crud news

---
+++ backend/modules/news/views/news/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\modules\news\models\db\News */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="news-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'tags')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'dt_add')->textInput() ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ common/models/db/News.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace common\models\db;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $tags
+ * @property string $text
+ * @property integer $dt_add
+ * @property integer $status
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'tags', 'text', 'dt_add', 'status'], 'required'],
+            [['text'], 'string'],
+            [['dt_add', 'status'], 'integer'],
+            [['title', 'tags'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'tags' => 'Tags',
+            'text' => 'Text',
+            'dt_add' => 'Dt Add',
+            'status' => 'Status',
+        ];
+    }
+}
